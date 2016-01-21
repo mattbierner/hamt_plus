58f2d1f0c5f82201205f9b8cb399c535f8919b11
Matt Bierner <mattbierner@gmail.com>
2016-01-21 14:16:13 -0800

    reenabme mutations do no effect each other test

--- tests/mutation.js
+++ tests/mutation.js
@@ -2,7 +2,7 @@
 const hamt = require('../hamt');
 const assert = require('chai').assert;
 
-describe('modify', () => {
+describe('mutate', () => {
   it('should insert single element into empty map', () => {
       const h1 = hamt.mutate(function(m) {
           hamt.set('a', 3, m);
@@ -58,31 +58,33 @@ describe('modify', () => {
       assert.strictEqual(5, hamt.get('b', h1));
   });
 
+  it('should not effect other mutations', () => {
+      const h = hamt.set('a', 100, hamt.make());
+
+      const h1 = hamt.mutate(function(m) {
+          hamt.set('a', 3, m);
+          hamt.set('b', 5, m);
+      }, h);
+
+      const h2 = hamt.mutate(function(m) {
+          hamt.set('a', 30, m);
+          hamt.set('b', 50, m);
+      }, h1);
+
+      assert.strictEqual(1, h.size);
+      assert.strictEqual(100, hamt.get('a', h));
+      assert.strictEqual(undefined, hamt.get('b', h));
+
+      assert.strictEqual(2, h1.size);
+      assert.strictEqual(3, hamt.get('a', h1));
+      assert.strictEqual(5, hamt.get('b', h1));
+
+      assert.strictEqual(2, h2.size);
+      assert.strictEqual(50, hamt.get('b', h2));
+      assert.strictEqual(30, hamt.get('a', h2));
+  });
+
 });
-
-exports.mutations_do_not_effect_one_another = function(test) {
-    const h = hamt.set('a', 100, hamt.make());
-
-    const h1 = hamt.mutate(function(m) {
-        hamt.set('a', 3, m);
-        hamt.set('b', 5, m);
-    }, h);
-
-    const h2 = hamt.mutate(function(m) {
-        hamt.set('a', 30, m);
-        hamt.set('b', 50, m);
-    }, h1);
-
-    assert.strictEqual(hamt.get('a', h), 100);
-    assert.strictEqual(hamt.get('a', h1), 3);
-    assert.strictEqual(hamt.get('a', h2), 30);
-
-    assert.strictEqual(hamt.get('b', h), null);
-    assert.strictEqual(hamt.get('b', h1), 5);
-    assert.strictEqual(hamt.get('b', h2), 50);
-
-
-};
 
 exports.many = function(test) {
     const insert = [
